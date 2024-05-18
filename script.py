72bc872bb55ecf3586355f7bd6ef2a59e17da146
Won Yang <yangwon700@gmail.com>
2024-05-18 18:07:33 -0500

    add different time ranges

--- script.py
+++ script.py
@@ -1,27 +1,33 @@
 import csv
 
-with open("./data/daily.csv", mode="r") as file:
-    csvFile = csv.DictReader(file)
+time_ranges = ["daily", "weekly", "monthly"]
 
-    wins = 0
-    losses = 0
+for time_range in time_ranges:
+    print("Time range:", time_range)
+    print("=====================================")
+    with open("./data/voo/" + time_range + ".csv", mode="r") as file:
+        csvFile = csv.DictReader(file)
 
-    sum_percent_wins = 0
-    sum_percent_losses = 0
+        wins = 0
+        losses = 0
 
-    for line in csvFile:
-        close_price = float(line["Close"])
-        open_price = float(line["Open"])
-        if close_price >= open_price:
-            wins += 1
-            percent_win = (close_price - open_price) / open_price * 100
-            sum_percent_wins += percent_win
-        else:
-            losses += 1
-            percent_loss = (open_price - close_price) / open_price * 100
-            sum_percent_losses += percent_loss
+        sum_percent_wins = 0
+        sum_percent_losses = 0
 
-    print("Sum percent wins:", sum_percent_wins)
-    print("Sum percent losses:", sum_percent_losses)
-    print("# wins:", wins)
-    print("# losses:", losses)
+        for line in csvFile:
+            close_price = float(line["Close"])
+            open_price = float(line["Open"])
+            if close_price >= open_price:
+                wins += 1
+                percent_win = (close_price - open_price) / open_price * 100
+                sum_percent_wins += percent_win
+            else:
+                losses += 1
+                percent_loss = (open_price - close_price) / open_price * 100
+                sum_percent_losses += percent_loss
+
+        print("Sum percent wins:", sum_percent_wins)
+        print("Sum percent losses:", sum_percent_losses)
+        print("# wins:", wins)
+        print("# losses:", losses)
+        print()
